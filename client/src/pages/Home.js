e2cbf15ff17ec1c204f5cbfbb3b028c4ff07090a
chizzy <cwchiz16@gmail.com>
2020-02-10 18:11:36 -0700

    changed nav

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -149,12 +149,11 @@ export default function Home() {
   return (
     <div>
       <div>
-          <nav className="navbar navbar-expand-lg">
+          <nav className="navbar navbar-expand-lg navbar-light">
     
             <a className="navbar-brand" href="#" >Hello, {user.nickname}!</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
-
             </button>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
